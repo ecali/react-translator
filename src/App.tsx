bd333f3cb4dc34287d2f7ed5a3de443d235fe71c
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-12-14 19:09:02 +0100

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import './App.css';
+import "./App.css";
+import { Header } from "./components/Header";
+import { Word } from "./components/Word";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <Header />
+        <Word />
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+    return(
+        <div className="header-container">
+            <h1>Hello 👋!</h1>
+            <h3>I can translate your words...</h3>
+        </div>
+    );
+}
